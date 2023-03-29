adc00c33973ef93eba772b7f3c1b0577fbe2fb84
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-29 10:42:07 +0100

    indentation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,30 +59,30 @@ export const Contact = () => {
             <TrackVisibility>
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn animate__repeat-1 animate__slow" : ""}>
-              <h2>Get in touch</h2>
-              <form onSubmit={handleSubmit}>
-                <Row>
-                  <Col xs={12} sm={6} className="px-1">
-                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                  </Col>
-                  <Col xs={12} sm={6} className="px-1">
-                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                  </Col>
-                  <Col xs={12} sm={6} className="px-1">
-                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                  </Col>
-                  <Col xs={12} className="px-1">
-                    <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                    <button type="submit"><span>{buttonText}</span></button>
-                  </Col>
-                  {
-                    status.message &&
-                    <Col>
-                      <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                <h2>Get in touch</h2>
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col xs={12} sm={6} className="px-1">
+                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
-                  }
-                </Row>
-              </form>
+                    <Col xs={12} sm={6} className="px-1">
+                      <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                    </Col>
+                    <Col xs={12} sm={6} className="px-1">
+                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                    </Col>
+                    <Col xs={12} className="px-1">
+                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <button type="submit"><span>{buttonText}</span></button>
+                    </Col>
+                    {
+                      status.message &&
+                      <Col>
+                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                      </Col>
+                    }
+                  </Row>
+                </form>
               </div>}
             </TrackVisibility>
           </Col>
